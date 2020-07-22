477789fda85574aa2007c7ae8fccbe3cfbe6e11c
minhduc <minh.duc.3701@gmail.com>
2020-07-22 22:53:32 +0700

    bread 70

--- src/Dependencies/Breadcrumb/BreadNode.tsx
+++ src/Dependencies/Breadcrumb/BreadNode.tsx
@@ -44,67 +44,37 @@ class BreadNode extends React.Component<IBreadNodesProps, INodeState> {
     );
   };
 
-  onRenderTreeNode = (
-    node: IBreadNodes[],
-    parentNode?: IBreadNodes,
-    onSelected?: (value: any) => void,
-    currentTree?: IBreadNodes[]
-  ) => {
-    return (
-      <div style={{ display: "flex" }}>
-        {node.map((item, index) => {
-          return (
-            <BreadNode
-              key={index}
-              child={item.child}
-              label={item.label}
-              src={item.src}
-              parentNode={parentNode}
-              node={item}
-              isSelected={item.isSelected || false}
-              onSelected={onSelected}
-              currentTree={currentTree}
-            />
-          );
-        })}
-      </div>
-    );
-  };
-
   onRenderNode = (props: IBreadNodesProps) => {
     const onSelectOption = (e?: any) => {
       const { value } = e.target;
       let selectedItem = JSON.parse(value);
       props.onSelected && props.onSelected(selectedItem);
     };
+
+    let selectNode = props.child.filter((node) => node.isSelected);
     return (
       <ItemWrapper>
         <BreadWrapper>
-          {(!props.parentNode ||
-            (props.child.length <= 1 &&
-              props.currentTree &&
-              props.currentTree.length < 1)) && (
-            <>
-              <ItemWrapper>
-                <a className="label-btn" href={props.src}>
-                  {props.label}
-                </a>
-                <Icon className="ms-breadIcon" iconName="ChevronRight" />
-              </ItemWrapper>
-            </>
+          {(!props.parentNode || props.child.length === 1) && (
+            <ItemWrapper>
+              <a className="label-btn" href={props.src}>
+                {props.label}
+              </a>
+              <Icon className="ms-breadIcon" iconName="ChevronRight" />
+            </ItemWrapper>
           )}
           {props.child.length > 1 && (
             <ItemWrapper>
               {props.parentNode && props.parentNode.child.length < 2 && (
-                <>
+                <ItemWrapper>
                   <a className="label-btn" href={props.src}>
                     {props.label}
                   </a>
                   <Icon className="ms-breadIcon" iconName="ChevronRight" />
-                </>
+                </ItemWrapper>
               )}
               <SelectWrapper>
-                <select id={`select-${props.label}`} onChange={onSelectOption}>
+                <select onChange={onSelectOption}>
                   {props.child.map((item, index) => {
                     return (
                       <option value={JSON.stringify(item)} key={index}>
@@ -114,19 +84,20 @@ class BreadNode extends React.Component<IBreadNodesProps, INodeState> {
                   })}
                 </select>
               </SelectWrapper>
+              {props.currentTree && props.currentTree.length < 1 && (
+                <Icon className="ms-breadIcon" iconName="ChevronRight" />
+              )}
             </ItemWrapper>
           )}
         </BreadWrapper>
-        {props.isSelected &&
+        {((props.child.length < 2 && selectNode && selectNode.length < 1) ||
+          (selectNode && selectNode.length >= 1)) &&
           this.onRenderSelectedNode(
-            props.child,
+            selectNode && selectNode.length >= 1 ? selectNode : props.child,
             props,
             props.onSelected,
             props.currentTree
           )}
-        {props.currentTree &&
-          props.currentTree.length > 0 &&
-          this.onRenderTreeNode(props.currentTree, props, props.onSelected)}
       </ItemWrapper>
     );
   };
--- src/Dependencies/Breadcrumb/index.tsx
+++ src/Dependencies/Breadcrumb/index.tsx
@@ -19,41 +19,40 @@ class Breadcrumd extends React.Component<IBreadcrumdProps, IBreadcrumdStates> {
 
   onSelectedChild = async (value: IBreadNodes, child?: IBreadNodes[]) => {
     let { NodesList } = this.state;
-    this.onSetSeletedTree(value);
-    // if (!child) {
-    //   for (let i = 0; i < NodesList.length; i++) {
-    //     if (NodesList[i].child.length >= 1) {
-    //       let nodeChild = NodesList[i].child;
-    //       for (let j = 0; j < nodeChild.length; j++) {
-    //         if (nodeChild[j].label === value.label) {
-    //           nodeChild[j].isSelected = true;
-    //           await this.onSetDefaultSelected(nodeChild, value);
-    //           this.onSetState(nodeChild[j]);
-    //         }
-    //       }
-    //       if (NodesList[i] !== value && NodesList[i].child.length >= 1) {
-    //         this.onSelectedChild(value, NodesList[i].child);
-    //       }
-    //     }
-    //   }
-    // }
-    // if (child) {
-    //   for (let i = 0; i < child.length; i++) {
-    //     if (child[i].child.length >= 1) {
-    //       let nodeChild = child[i].child;
-    //       for (let j = 0; j < nodeChild.length; j++) {
-    //         if (nodeChild[j].label === value.label) {
-    //           nodeChild[j].isSelected = true;
-    //           await this.onSetDefaultSelected(nodeChild, value);
-    //           this.onSetState(nodeChild[j]);
-    //         }
-    //       }
-    //       if (child[i] !== value && child[i].child.length >= 1) {
-    //         this.onSelectedChild(value, child[i].child);
-    //       }
-    //     }
-    //   }
-    // }
+    if (!child) {
+      for (let i = 0; i < NodesList.length; i++) {
+        if (NodesList[i].child.length >= 1) {
+          let nodeChild = NodesList[i].child;
+          for (let j = 0; j < nodeChild.length; j++) {
+            if (nodeChild[j].label === value.label) {
+              nodeChild[j].isSelected = true;
+              await this.onSetDefaultSelected(nodeChild, value);
+              this.onSetState(nodeChild[j]);
+            }
+          }
+          if (NodesList[i] !== value && NodesList[i].child.length >= 1) {
+            this.onSelectedChild(value, NodesList[i].child);
+          }
+        }
+      }
+    }
+    if (child) {
+      for (let i = 0; i < child.length; i++) {
+        if (child[i].child.length >= 1) {
+          let nodeChild = child[i].child;
+          for (let j = 0; j < nodeChild.length; j++) {
+            if (nodeChild[j].label === value.label) {
+              nodeChild[j].isSelected = true;
+              await this.onSetDefaultSelected(nodeChild, value);
+              this.onSetState(nodeChild[j]);
+            }
+          }
+          if (child[i] !== value && child[i].child.length >= 1) {
+            this.onSelectedChild(value, child[i].child);
+          }
+        }
+      }
+    }
   };
 
   onSetDefaultSelected = (node: IBreadNodes[], value: IBreadNodes) => {
@@ -81,56 +80,57 @@ class Breadcrumd extends React.Component<IBreadcrumdProps, IBreadcrumdStates> {
   };
 
   //doing with for in for
-  onCheckIncludes = async (value: IBreadNodes, childRepo?: IBreadNodes[]) => {
-    let currentTree = this.state.mySelectTree;
-    let result = false;
-    if (!childRepo) {
-      for (let i = 0; i < currentTree.length; i++) {
-        if (currentTree[i].label === value.label) {
-          console.log("find");
-        }
-        if (
-          currentTree[i].label !== value.label &&
-          currentTree[i].child.length >= 1
-        ) {
-          let child = currentTree[i].child;
-          for (let j = 0; j < child.length; j++) {
-            if (child[j].label === value.label) {
-              console.log(currentTree[i]);
-              console.log("find");
-            }
-            if (child[j].label !== value.label && child[j].child.length >= 1) {
-              this.onCheckIncludes(value, child[j].child);
-            }
-          }
-        }
-      }
-    }
-    if (childRepo) {
-      for (let i = 0; i < childRepo.length; i++) {
-        if (childRepo[i].label === value.label) {
-          console.log("find");
-        }
-        if (
-          childRepo[i].label !== value.label &&
-          childRepo[i].child.length >= 1
-        ) {
-          let child = childRepo[i].child;
-          for (let j = 0; j < child.length; j++) {
-            if (child[j].label === value.label) {
-              console.log(childRepo[i]);
-              console.log("find");
-            }
-            if (child[j].label !== value.label && child[j].child.length >= 1) {
-              this.onCheckIncludes(value, child[j].child);
-            }
-          }
-        }
-      }
-    }
-  };
+  // onCheckIncludes = async (value: IBreadNodes, childRepo?: IBreadNodes[]) => {
+  //   let currentTree = this.state.mySelectTree;
+  //   let result = false;
+  //   if (!childRepo) {
+  //     for (let i = 0; i < currentTree.length; i++) {
+  //       if (currentTree[i].label === value.label) {
+  //         console.log("find");
+  //       }
+  //       if (
+  //         currentTree[i].label !== value.label &&
+  //         currentTree[i].child.length >= 1
+  //       ) {
+  //         let child = currentTree[i].child;
+  //         for (let j = 0; j < child.length; j++) {
+  //           if (child[j].label === value.label) {
+  //             console.log(currentTree[i]);
+  //             console.log("find");
+  //           }
+  //           if (child[j].label !== value.label && child[j].child.length >= 1) {
+  //             this.onCheckIncludes(value, child[j].child);
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  //   if (childRepo) {
+  //     for (let i = 0; i < childRepo.length; i++) {
+  //       if (childRepo[i].label === value.label) {
+  //         console.log("find");
+  //       }
+  //       if (
+  //         childRepo[i].label !== value.label &&
+  //         childRepo[i].child.length >= 1
+  //       ) {
+  //         let child = childRepo[i].child;
+  //         for (let j = 0; j < child.length; j++) {
+  //           if (child[j].label === value.label) {
+  //             console.log(childRepo[i]);
+  //             console.log("find");
+  //           }
+  //           if (child[j].label !== value.label && child[j].child.length >= 1) {
+  //             this.onCheckIncludes(value, child[j].child);
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // };
 
   render() {
+    console.log(this.state.NodesList);
     return (
       <div>
         {this.state.NodesList.map((item, index) => {
